04fe7d8470deb4acbb0301814e5e004af0104b93
rocha <rjoao0112@gmail.com>
2022-11-16 10:37:52 +0000

    Bidimentional Array Class

--- src/BidimentionalArray.java
+++ src/BidimentionalArray.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class BidimentionalArray {
+import java.util.Arrays;
+
+public class BidimentionalArray {
+
+    private static final int[][] bidiArray = new int[][]{{0,0},{0,1},{1,0},{1,1},{2,0},{2,1},{3,0},{3,1}};
+
+    public static void print(){
+        for (int[] i : bidiArray){
+
+            System.out.println(Arrays.toString(i));
+            System.out.println();
+        }
+    }
 }
