09d0aa2c5c1c19672f08fbf46f691bb3781a7934
rocha <rjoao0112@gmail.com>
2022-11-16 10:47:08 +0000

    Bidimentional Array Class Correction

--- src/BidimentionalArray.java
+++ src/BidimentionalArray.java
@@ -1,14 +1,18 @@
-import java.util.Arrays;
-
 public class BidimentionalArray {
 
-    private static final int[][] bidiArray = new int[][]{{0,0},{0,1},{1,0},{1,1},{2,0},{2,1},{3,0},{3,1}};
+    private static final int[][] bidiArray = new int[][]{{0,0},{0,1},{1,0},{1,1},{2,0},{2,1}};
 
     public static void print(){
-        for (int[] i : bidiArray){
-
-            System.out.println(Arrays.toString(i));
-            System.out.println();
+        for (int i = 0; i < bidiArray.length; i++){
+            System.out.print("{ ");
+            for (int f = 0; f < bidiArray[i].length; f++){
+                if(f == 0){
+                    System.out.print(bidiArray[i][f] + " ,");
+                    continue;
+                }
+                System.out.print(bidiArray[i][f]);
+            }
+            System.out.println(" }");
         }
     }
 }
